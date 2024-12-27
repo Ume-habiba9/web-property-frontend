3efa345109e62a91378967db7abb95b0a25da540
Zuno Alvi <zunnooran.alvi@gmail.com>
2024-12-27 07:04:01 +0500

    feat:changes

--- src/pages/find -right-property/find-right-property.tsx
+++ src/pages/find -right-property/find-right-property.tsx
@@ -1,5 +1,15 @@
+import HeroSection from 'pages/home/components/hero-section';
+
+import heroImg2 from 'assets/images/hero2.png';
+
+const images = [heroImg2];
+
 function FindTheRightProperty() {
-  return <div>Property</div>;
+  return (
+    <div>
+      <HeroSection images={images} />
+    </div>
+  );
 }
 
 export default FindTheRightProperty;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,15 +5,22 @@ import NewsletterSection from 'components/layout/components/newsletter-section';
 import QueriesSection from 'components/layout/components/queriesSection';
 import Stats from 'components/layout/components/stats';
 
+// Import location icon
+import heroImg1 from 'assets/images/hero1.png';
+import heroImg2 from 'assets/images/hero2.png';
+import heroImg3 from 'assets/images/hero3.png';
+
 import PropertySection from './components/PropertySection';
 import AvatarSection from './components/avatar-section';
 import HeroSection from './components/hero-section';
 import TouringSection from './components/touring-section';
 
+const images = [heroImg1, heroImg2, heroImg3];
+
 function Home() {
   return (
     <div>
-      <HeroSection />
+      <HeroSection images={images} />
       <Container>
         <AvatarSection />
       </Container>
